951f5f91b690dbd89bc61d6d0052df3e017d5384
17kumarrupesh <55622346+17kumarrupesh@users.noreply.github.com>
2024-06-20 07:30:56 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,4 @@
 # Configure Azure Provider
 provider "azurerm" {
   features {}
-
-  # Use environment variables for credentials
-  client_id       = env("ARM_CLIENT_ID")
-  client_secret   = env("ARM_CLIENT_SECRET")
-  subscription_id = env("ARM_SUBSCRIPTION_ID")
-  tenant_id       = env("ARM_TENANT_ID")
 }
